b44321ec14ab0f35deeaf77e85845eeb0de640e2
Nachiketa Nalin <nalinnachiketa@gmail.com>
2022-02-16 01:19:28 +0530

    Updates

---
+++ precache-manifest.f9010047bfe5a440a3bb5e78d521e8ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdf3c2ffc23d00cd7a06e0055bca5e9",
+    "url": "/Note-Keeper/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Note-Keeper/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "d25798fbd8a6b7188ebe",
+    "url": "/Note-Keeper/static/js/main.86f53bda.chunk.js"
+  },
+  {
+    "revision": "2b252a5abc1915b56f15",
+    "url": "/Note-Keeper/static/js/runtime-main.0e255702.js"
+  }
+]);
