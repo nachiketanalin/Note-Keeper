85c78a9e5f249977f6e63df460153c47c2df15f1
Nachiketa Nalin <nalinnachiketa@gmail.com>
2022-02-16 01:18:17 +0530

    bugs fixed

---
+++ build/precache-manifest.b6a37c15a8ca8d2708d83d6675f3f846.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47d791fa1c2475deb36d5af473f83b6",
+    "url": "/Note-Keeper/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Note-Keeper/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "d25798fbd8a6b7188ebe",
+    "url": "/Note-Keeper/static/js/main.86f53bda.chunk.js"
+  },
+  {
+    "revision": "2b252a5abc1915b56f15",
+    "url": "/Note-Keeper/static/js/runtime-main.0e255702.js"
+  }
+]);
